aa8055caa20832d1060ff6bb6d140eaf62f2aab7
Tomer Filiba <tomer@sweet.security>
2024-08-12 08:26:55 +0300

    Refactor position_simd to eliminate bugs

--- src/shard.rs
+++ src/shard.rs
@@ -33,6 +33,19 @@ pub(crate) struct ShardRow {
     pub offsets_and_sizes: [u64; ROW_WIDTH], // | key_size: 16 | val_size: 16 | file_offset: 32 |
 }
 
+impl ShardRow {
+    #[inline]
+    fn lookup(&self, sig: u32, start_idx: &mut usize) -> Option<usize> {
+        if let Some(rel_idx) = self.signatures[*start_idx..].iter().position_simd(sig) {
+            let abs_idx = rel_idx + *start_idx;
+            *start_idx = abs_idx + 1;
+            Some(abs_idx)
+        } else {
+            None
+        }
+    }
+}
+
 #[repr(C, align(4096))]
 pub(crate) struct PageAligned<T>(pub T);
 
@@ -78,15 +91,11 @@ pub(crate) type KVPair = (Vec<u8>, Vec<u8>);
 impl<'a> Iterator for ByHashIterator<'a> {
     type Item = Result<KVPair>;
     fn next(&mut self) -> Option<Self::Item> {
-        while let Some(rel_idx) = self.row.signatures[self.start_idx..]
-            .iter()
-            .position_simd(self.signature)
-        {
-            let abs_idx = self.start_idx + rel_idx;
-            self.start_idx = abs_idx + 1;
-            return Some(self.shard.read_kv(self.row.offsets_and_sizes[abs_idx]));
+        if let Some(idx) = self.row.lookup(self.signature, &mut self.start_idx) {
+            Some(self.shard.read_kv(self.row.offsets_and_sizes[idx]))
+        } else {
+            None
         }
-        None
     }
 }
 
@@ -264,9 +273,8 @@ impl Shard {
         mode: InsertMode,
     ) -> Result<TryReplaceStatus> {
         let mut start = 0;
-        while let Some(rel_idx) = row.signatures[start..].iter().position_simd(ph.signature()) {
-            let abs_idx = start + rel_idx;
-            let (k, v) = self.read_kv(row.offsets_and_sizes[abs_idx])?;
+        while let Some(idx) = row.lookup(ph.signature(), &mut start) {
+            let (k, v) = self.read_kv(row.offsets_and_sizes[idx])?;
             if key == k {
                 match mode {
                     InsertMode::GetOrCreate => {
@@ -276,7 +284,7 @@ impl Shard {
                     InsertMode::Set | InsertMode::Replace => {
                         // optimization
                         if val != v {
-                            row.offsets_and_sizes[abs_idx] = self.write_kv(key, val)?;
+                            row.offsets_and_sizes[idx] = self.write_kv(key, val)?;
                             self.header
                                 .wasted_bytes
                                 .fetch_add((k.len() + v.len()) as u64, Ordering::SeqCst);
@@ -285,7 +293,6 @@ impl Shard {
                     }
                 }
             }
-            start = abs_idx + 1;
         }
         Ok(TryReplaceStatus::KeyDoesNotExist)
     }
@@ -331,7 +338,8 @@ impl Shard {
                 }
 
                 // find an empty slot
-                if let Some(idx) = row.signatures.iter().position_simd(INVALID_SIG) {
+                let mut start = 0;
+                if let Some(idx) = row.lookup(INVALID_SIG, &mut start) {
                     let new_off = self.write_kv(&full_key, val)?;
 
                     // we don't want a reorder to happen here - first write the offset, then the signature
@@ -377,13 +385,11 @@ impl Shard {
         let (_guard, row) = self.get_row_mut(ph);
 
         let mut start = 0;
-        while let Some(rel_idx) = row.signatures[start..].iter().position_simd(ph.signature()) {
-            let abs_idx = start + rel_idx;
-            let (k, v) = self.read_kv(row.offsets_and_sizes[abs_idx])?;
+        while let Some(idx) = row.lookup(ph.signature(), &mut start) {
+            let (k, v) = self.read_kv(row.offsets_and_sizes[idx])?;
             if key == k {
-                return func(&self, row, ph, Some((abs_idx, k, v)));
+                return func(&self, row, ph, Some((idx, k, v)));
             }
-            start = abs_idx + 1;
         }
 
         func(&self, row, ph, None)
