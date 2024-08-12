33cd0b99fb5b7f66e61c63d843221169bc7d4f5f
Tomer Filiba <tomer@sweet.security>
2024-08-12 08:38:31 +0300

    Add a short version of the longliving test to CI

--- vicky-longliving/src/main.rs
+++ vicky-longliving/src/main.rs
@@ -1,27 +1,38 @@
-use std::sync::Arc;
+use std::sync::{atomic::AtomicU64, Arc};
 
 use vicky_store::{Config, Result, VickyStore, VickyTypedList};
 
 fn main() -> Result<()> {
+    let args = std::env::args().collect::<Vec<_>>();
+    assert!(args.len() == 2, "expected a single count argument");
+    let size = args
+        .get(1)
+        .expect("count argument required")
+        .parse()
+        .expect("count must be a usize");
+
     let db = Arc::new(VickyStore::open("dbdir", Config::default())?);
     db.clear()?;
 
     let mut handles = vec![];
 
-    const SIZE: usize = 50_000;
+    let ops = Arc::new(AtomicU64::new(0));
 
     for thd in 0..10 {
         let db = db.clone();
+        let ops = ops.clone();
         let h = std::thread::spawn(move || {
             println!("started thread {thd}");
             let typed = VickyTypedList::<String, usize, String>::new(db.clone());
-            for i in 0..SIZE {
+            for i in 0..size {
                 if i % 10000 == 0 {
                     println!("thread {thd} at {i} {:?}", db.stats());
                 }
-                typed.set("mylist".into(), thd * SIZE + i, "xxx".into())?;
+                typed.set("mylist".into(), thd * size + i, "xxx".into())?;
+                ops.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
                 if i > 1000 {
-                    typed.remove("mylist".into(), &(thd * SIZE + i - 1000))?;
+                    typed.remove("mylist".into(), &(thd * size + i - 1000))?;
+                    ops.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
                 }
             }
 
@@ -33,6 +44,7 @@ fn main() -> Result<()> {
     for h in handles {
         h.join().unwrap()?;
     }
+    println!("ops={}", ops.load(std::sync::atomic::Ordering::Relaxed));
 
     Ok(())
 }
